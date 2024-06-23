691b6fbc9b73dab0054ccf885dae2227361d6d4a
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-06-23 14:10:34 +0500

    player naming is working

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -2,23 +2,35 @@ import { useState } from "react";
 
 export default function PlayerInfo({ playerName, playerSymbol }) {
   let [editing, isEditing] = useState(false);
+  let [initialPlayerName, editPlayerName] = useState(playerName);
 
-  function buttonHander() {
+  function buttonHandler() {
     isEditing((inverse) => !inverse);
     console.log(editing);
   }
 
-  let playerElement = <span className="player-name">{playerName}</span>;
+  function inputHandler(event) {
+    editPlayerName(event.target.value);
+  }
+
+  let playerElement = <span className="player-name">{initialPlayerName}</span>;
   let buttonName = "Edit";
   if (editing) {
-    playerElement = <input type="text"></input>;
+    playerElement = (
+      <input
+        type="text"
+        required
+        value={initialPlayerName}
+        onChange={inputHandler}
+      ></input>
+    );
     buttonName = "Save";
   }
   return (
     <li className="player">
       {playerElement}
       <span className="player-symbol">{playerSymbol}</span>
-      <button onClick={buttonHander}>{buttonName}</button>
+      <button onClick={buttonHandler}>{buttonName}</button>
     </li>
   );
 }
